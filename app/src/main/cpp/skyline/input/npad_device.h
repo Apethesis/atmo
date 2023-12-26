daa4919045c48c86e3914493304f84e2b59f89f4
Apethesis <apethesis@gmail.com>
2023-12-26 13:05:57 +0100

    wananananananana batman

--- app/src/main/cpp/skyline/input/npad_device.h
+++ app/src/main/cpp/skyline/input/npad_device.h
@@ -64,14 +64,14 @@ namespace skyline::input {
             bool nes : 1; //!< NES controller
             bool nesHandheld : 1; //!< NES controller in handheld mode
             bool snes : 1; //!< SNES controller
-            bool n64 : 1;
-            bool segaGenesis : 1;
+            bool n64 : 1; //!< N64 controller 
+            bool segaGenesis : 1; //!< Sega Genesis controller
             u16 reserved0;
-            bool genericExternal : 1;
-            bool generic : 1;
+            bool genericExternal : 1; //!< External Generic controller
+            bool generic : 1; //!< Generic controller
         };
     };
-    static_assert(sizeof(NpadStyleSet) == 0x4);
+    static_assert(sizeof(NpadStyleSet) == 0x8);
 
     enum class NpadAxisId {
         LX, //!< Left Stick X
